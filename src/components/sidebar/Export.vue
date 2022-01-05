8af755c42eacc273bb8a6aa48f0b2c3196709e2d
Arunmozhi <arun@arunmozhi.in>
2022-01-05 23:49:06 +0530

    converts to desktop app with filesave

--- src/components/sidebar/Export.vue
+++ src/components/sidebar/Export.vue
@@ -5,33 +5,42 @@
     </button>
   </div>
 </template>
+
 <script>
-import {mapState} from "vuex";
+import { mapState } from "vuex";
+import { save } from "@tauri-apps/api/dialog";
+import { invoke } from "@tauri-apps/api/tauri";
+import { documentDir } from "@tauri-apps/api/path";
+
 export default {
   name: "Export",
   computed: {
     ...mapState(["annotations", "classes"]),
   },
   methods: {
-    generateJSONExport() {
-      const filename = "training_data.json";
+    async generateJSONExport() {
       const output = {
-        "classes": this.classes.map(c => c.name),
-        "annotations": this.annotations
-      }
+        classes: this.classes.map((c) => c.name),
+        annotations: this.annotations,
+      };
       const jsonStr = JSON.stringify(output);
 
-      let element = document.createElement("a");
-      element.setAttribute(
-        "href",
-        "data:text/plain;charset=utf-8," + encodeURIComponent(jsonStr)
-      );
-      element.setAttribute("download", filename);
+      save({
+        defaultPath: await documentDir(),
+        filters: [
+          { extensions: ["json"], name: "JSON Files (*.json)" },
+          { name: "All files (*.*)", extensions: ["*"] },
+        ],
+      })
+        .then((path) => {
+          if (!path) return;
+          if (!path.match(/.*\.json$/)) path += ".json";
 
-      element.style.display = "none";
-      document.body.appendChild(element);
-      element.click();
-      document.body.removeChild(element);
+          invoke("save_file", { filepath: path, contents: jsonStr })
+            .then((msg) => alert(msg))
+            .catch((e) => alert(e));
+        })
+        .catch((e) => console.log("Save cancelled.", e));
     },
   },
 };
