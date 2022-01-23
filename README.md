c4fe56208241526163a4f11f9b9c539dd4402317
Arunmozhi <arun@arunmozhi.in>
2022-01-23 14:38:22 +0530

    replaced the icons and updated readme

--- README.md
+++ README.md
@@ -1,24 +1,42 @@
-# ui
+# NER Annotator for Spacy
 
-## Project setup
-```
+NER Annotator for SpaCy allows you to create training data for creating a custome NER Model with custom tags. It can either be access as a web application directly from [https://tecoholic.github.io/ner-annotator/](https://tecoholic.github.io/ner-annotator/) or can be installed in Linux systems as a desktop application.
+
+## Desktop application (experimental)
+
+An experimental version of the annotator is also available for Linux as:
+
+- [Debian Package](https://github.com/tecoholic/ner-annotator/releases/download/0.1.0/ner-annotator_0.1.0_amd64.deb)
+- [AppImage](https://github.com/tecoholic/ner-annotator/releases/download/0.1.0/ner-annotator_0.1.0_amd64.AppImage)
+
+## Screenshots of the process
+
+![Homepage](assets/start_page.png?raw=true)
+![Annotation Page](assets/tagging.png?raw=true)
+![JSON output](assets/output.png?raw=true)
+
+## Development
+
+### Running it locally for development
+
+1. Open another terminal and start the server for the UI
+
+```sh
+cd ui
 yarn install
-```
-
-### Compiles and hot-reloads for development
-```
 yarn serve
 ```
 
-### Compiles and minifies for production
-```
-yarn build
+Now go to [http://localhost:8081/ner-annotator/](http://localhost:8081/ner-annotator/)
+
+### Developing the desktop application
+
+The desktop applications have been created using [Tauri](https://tauri.studio).
+
+```sh
+yarn tauri:serve
 ```
 
-### Lints and fixes files
-```
-yarn lint
-```
+## Credits
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+1. App Icon - <a href="https://www.flaticon.com/free-icons/ornithology" title="ornithology icons">Ornithology icons created by Freepik - Flaticon</a>
--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -4,7 +4,7 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">NER Text Annotator</h1>
-          <h2 class="subtitle">Annotate text for SpaCy NER Model training</h2>
+          <h2 class="subtitle">Annotate text for spaCy NER Model training</h2>
         </div>
       </div>
     </section>
@@ -15,96 +15,8 @@
           <div class="column">
             <LoadTextFile class="pt-6" v-on:file-loaded="onFileLoaded" />
           </div>
-          <div class="column">
-            <figure class="image" style="max-height: 15rem;">
-              <img src="../assets/undraw_annotation.svg" />
-            </figure>
-          </div>
         </div>
       </div>
-    </section>
-
-    <section id="explanation" class="hero is-light">
-      <div class="hero-body">
-        <div class="container">
-
-          <h1 class="title">How to use?</h1>
-
-          <div class="columns">
-            <div class="column is-narrow">
-              <button class="button is-rounded is-small is-dark">1</button>
-            </div>
-            <div class="column">
-              <p class="is-size-5"><strong>Prepare and upload the input file</strong></p>
-              <p>Put all the text that needs to be annotated into a text file. If the corpus is large, split it into multiple files.</p>
-              <p>Each line will be presented as an entry for annotating one by one.</p>
-            </div>
-          </div>
-          <div class="columns">
-            <div class="column is-narrow">
-              <button class="button is-rounded is-small is-dark">2</button>
-            </div>
-            <div class="column">
-              <p class="is-size-5"><strong>Create Tags and annotate</strong></p>
-              <p>You can create any number of custom tags to annotate your text. The text will be presented as tokens for easy tagging.</p>
-              <p>You can start your selection anywhere on a word and end anywhere to tag a word. No character level accuracy is needed.</p>
-            </div>
-          </div>
-          <div class="columns">
-            <div class="column is-narrow">
-              <button class="button is-rounded is-small is-dark">3</button>
-            </div>
-            <div class="column">
-              <p class="is-size-5"><strong>Export your annotations as JSON</strong></p>
-              <p>The annotations are exported into a JSON array with the format:</p>
-              <pre><code>
-{
-  "classes": [...],
-  "annotations": [
-    ["raw_text", {"entities": [(start, end, class),....]}],
-    ...
-  ]
-}
-              </code></pre>
-            </div>
-          </div>
-          <div class="columns">
-            <div class="column is-narrow">
-              <button class="button is-rounded is-small is-dark">4</button>
-            </div>
-            <div class="column">
-              <p class="is-size-5"><strong>Use the JSON for training your model</strong></p>
-              <pre><code>
-import json
-import spacy
-
-# load the training data
-with open('your-annotations.json') as fp:
-  training_data = json.load(fp)
-
-# prepare an empty model to train
-nlp = spacy.blank('en')
-nlp.vocab.vectors.name = 'demo'
-ner = nlp.create_pipe('ner')
-nlp.add_pipe(ner, last=True)
-
-# Add the custome NER Tags as entities into the model
-for label in training_data["classes"]:
-  nlp.entity.add_label(label)
-
-# Train the model
-optimizer = nlp.begin_training()
-
-for text, annotations in training_data["annotations"]:
-    if len(text) > 0: # in case an empty sentence was saved while annotating
-        nlp.update([text], [annotations], sgd=optimizer)
-             </code></pre>
-
-            </div>
-          </div>
-        </div>
-      </div>
-
     </section>
   </div>
 </template>
