c778a6cf65f9b6de4dd239a1918362aa2f8cda2a
Alvi Khan <55395950+alvi-khan@users.noreply.github.com>
2024-07-03 21:40:34 +0600

    Allow annotation uploads from home.
    Closes #103.

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -18,26 +18,53 @@
       Annotate text for spaCy NER Model training
     </p>
 
-    <div
-      class="q-my-xl q-py-md"
-      style="margin-top: 7rem"
-    >
-      <q-file
-        v-model="textFile"
-        accept=".txt"
-        filled
-        label="Open a text file to begin"
-        :bg-color="$q.dark.isActive ? 'black-1' : 'light-blue-1'"
-        @rejected="fileSelectionError"
-        @update:model-value="onFileSelected"
-      >
-        <template #prepend>
-          <q-icon name="fas fa-upload" />
-        </template>
-      </q-file>
-      <p :class="['text-subtitle1 q-my-md', $q.dark.isActive ? 'text-grey-4' : 'text-grey-7']">
-        Hint: You can also drag and drop files into this window!
-      </p>
+    <div class="q-my-xl q-py-md column">
+      <div class="row justify-between items-center">
+        <div>
+          <p :class="['text-h6 q-my-md', $q.dark.isActive ? 'text-grey-4' : 'text-grey-7']">
+            Select your file(s), then press 'Begin'!
+          </p>
+          <p :class="['text-subtitle1 q-my-md', $q.dark.isActive ? 'text-grey-4' : 'text-grey-7']">
+            Hint: You can also drag and drop files into this window!
+          </p>
+        </div>
+        <q-btn
+          v-close-popup
+          outline
+          label="Begin!"
+          :color="!textFile ? 'grey-7' : 'blue'"
+          :disable="!textFile"
+          @click="onConfirmation"
+        />
+      </div>
+      <div class="row items-center">
+        <q-file
+          class="col-5 q-mx-sm"
+          v-model="textFile"
+          accept=".txt"
+          filled
+          label="Text file"
+          :bg-color="$q.dark.isActive ? 'black-1' : 'light-blue-1'"
+          @rejected="fileSelectionError('Invalid text file')"
+        >
+          <template #prepend>
+            <q-icon name="fas fa-upload" />
+          </template>
+        </q-file>
+        <q-file
+          class="col-5 q-mx-sm"
+          v-model="annotationFile"
+          accept=".json"
+          filled
+          label="(Optional) Annotations"
+          :bg-color="$q.dark.isActive ? 'black-1' : 'light-blue-1'"
+          @rejected="fileSelectionError('Invalid annotation file')"
+        >
+          <template #prepend>
+            <q-icon name="fas fa-upload" />
+          </template>
+        </q-file>
+      </div>
     </div>
   </div>
   <q-separator />
@@ -153,26 +180,45 @@ export default {
   data() {
     return {
       textFile: null,
+      annotationFile: null
     };
   },
   methods: {
-    ...mapMutations(["setInputSentences"]),
-    onFileSelected(file) {
+    ...mapMutations(["setInputSentences", "loadAnnotations"]),
+    onConfirmation() {
       try {
         let reader = new FileReader();
         reader.addEventListener("load", (event) => {
           this.setInputSentences(event.target.result);
-          this.$emit("file-loaded");
         });
-        reader.readAsText(file);
+        reader.readAsText(this.textFile);
       } catch(e) {
-        this.fileSelectionError();
+        this.fileSelectionError('Invalid text file');
+        return;
       }
+
+      if (!this.annotationFile)  {
+        this.$emit("file-loaded");
+        return
+      }
+
+      try {
+        let reader = new FileReader();
+        reader.addEventListener("load", (event) => {
+          this.loadAnnotations(JSON.parse(event.target.result));
+        });
+        reader.readAsText(this.annotationFile);
+      } catch(e) {
+        this.fileSelectionError('Invalid annotation file');
+        return;
+      }
+
+      this.$emit("file-loaded");
     },
-    fileSelectionError() {
+    fileSelectionError(msg) {
       this.$q.notify({
         icon: "fas fa-exclamation-circle",
-        message: "Invalid file",
+        message: msg,
         color: "red-6",
         position: "top",
         timeout: 2000,
