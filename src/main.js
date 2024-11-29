2e1267414c1a89763a999157aba4732a55173568
Arunmozhi <arun@arunmozhi.in>
2024-11-29 12:18:12 +1100

    refactor: upgrade dependencies
    - Remove webpack config
    - Install the latest quasar library with vite plugin
    - Update the SaSS imports
    - Add APPLICATION_VERSION to the vite config
    - update Github Actions to recent versions with Ubuntu 22 for Linux
      build and Node Version 21.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 import { createApp } from "vue";
+import { Quasar } from "quasar";
+import "@quasar/extras/material-icons/material-icons.css";
+import "quasar/src/css/index.sass";
+
 import App from "./App.vue";
 import "es6-promise/auto";
 import { createStore } from "vuex";
 import store from "./store";
 
-import { Quasar } from "quasar";
 import quasarUserOptions from "./quasar-user-options";
-import './assets/styles.scss';
-import './styles/quasar.scss';
-import './styles/quasar.variables.scss';
-import 'quasar/src/css/index.sass';
+import "./assets/styles.scss";
+import "./styles/quasar.scss";
 
 const app = createApp(App)
-    .use(Quasar, quasarUserOptions)
-    .use(createStore(store));
+  .use(Quasar, quasarUserOptions)
+  .use(createStore(store));
 
 app.mount("#app");
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,20 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import vueDevTools from "vite-plugin-vue-devtools";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: fileURLToPath(
+        new URL("./src/styles/quasar.variables.scss", import.meta.url)
+      ),
+    }),
     vueJsx(),
     vueDevTools(),
   ],
+  define: {
+    APPLICATION_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
-})
+});
