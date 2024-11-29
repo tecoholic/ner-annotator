e84e95a6e78a7fa87c8c587ed6f1126f5bcd3271
Arunmozhi <arun@arunmozhi.in>
2024-11-29 13:00:50 +1100

    feat: migrate tauri to version 2

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,6 +6,8 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
+const host = process.env.TAURI_DEV_HOST;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -27,5 +29,25 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  server: {
+    // Tauri expects a fixed port, fail if that port is not available
+    strictPort: true,
+    // if the host Tauri is expecting is set, use it
+    host: host || false,
+    port: 5173,
+  },
+  // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+  envPrefix: ["VITE_", "TAURI_ENV_*"],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
 });
