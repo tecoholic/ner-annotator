32eead5939ed2d797f12f86951d3c45dc19f09ae
Arunmozhi <arun@arunmozhi.in>
2024-11-30 19:36:47 +1100

    refactor: improve the about dialog information

--- src/components/AboutDialog.vue
+++ src/components/AboutDialog.vue
@@ -1,13 +1,8 @@
 <template>
-  <q-dialog
-    :model-value="show"
-    @hide="$emit('hide')"
-  >
+  <q-dialog :model-value="show" @hide="$emit('hide')">
     <q-card style="min-width: 400px; padding: 10px; border-radius: 10px">
       <q-card-section>
-        <div class="text-h6">
-          NER Annotator
-        </div>
+        <div class="text-h6">NER Annotator</div>
       </q-card-section>
 
       <q-card-section class="q-pt-none">
@@ -19,19 +14,23 @@
           </a>
         </p>
         <p>
-          <strong>Author:</strong>
-          @tecoholic (<a href="https://github.com/tecoholic">Github</a>,
-          <a href="https://github.com/tecoholic">Twitter</a>)
+          <strong>Contributors: </strong>
+          <a
+            href="https://github.com/tecoholic/ner-annotator/graphs/contributors"
+          >
+            See on Github
+          </a>
         </p>
+        <ul>
+          <li>Author: <a href="https://github.com/tecoholic">tecoholic</a></li>
+          <li>
+            Maintainer: <a href="https://github.com/alvi-khan">alvi-khan</a>
+          </li>
+        </ul>
       </q-card-section>
 
       <q-card-actions align="right">
-        <q-btn
-          v-close-popup
-          flat
-          label="OK"
-          color="primary"
-        />
+        <q-btn v-close-popup flat label="OK" color="primary" />
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -47,7 +46,7 @@ export default {
     },
   },
   emits: ["hide"],
-  data: function() {
+  data: function () {
     return {
       // eslint-disable-next-line no-undef
       version: APPLICATION_VERSION,
